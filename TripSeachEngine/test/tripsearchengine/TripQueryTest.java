5555a9bcf6438ba61472f9d298305a0719d0f492
arnargisla <arnargisla@gmail.com>
2015-03-11 17:33:41 +0000

    Added test folder

---
+++ TripSeachEngine/test/tripsearchengine/TripQueryTest.java
@@ -0,0 +1,21 @@
+package tripsearchengine;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TripQueryTest {
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@After
+	public void tearDown(){
+		
+	}
+	
+	@Test
+	public void testSomeBehavior(){
+		System.out.println("Halló Halldór!");
+	}
+}
