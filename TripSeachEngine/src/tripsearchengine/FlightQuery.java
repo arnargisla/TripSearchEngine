d3e9b64fcbc987a0ed63018305ea4ac85e137be1
aga33 <tonigeir@gmail.com>
2015-03-11 19:01:03 +0000

    Updated flightquery

--- TripSeachEngine/src/tripsearchengine/FlightQuery.java
+++ TripSeachEngine/src/tripsearchengine/FlightQuery.java
@@ -11,4 +11,49 @@ public class FlightQuery {
 	public void setDepartureDate(Date departureDate) {
 		// TODO Auto-generated method stub
 	}
+
+	public void setReturnDate(Date returnDate) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDepartureLocation(String departureLocation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setReturnLocation(String returnLocation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDestinationLocation(String destinationLocation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setMinPrice(int minPrice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setMaxPrice(int maxPrice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setStopoverDuration(double stopoverDuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setLayoverDuration(double layoverDuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setFlightClass(String flightClass) {
+		// TODO Auto-generated method stub
+		
+	}
 }
